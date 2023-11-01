60f9441ecf210a836455f50982d6731dcd793747
amaliyamiriyeva8 <amaliyamiriyeva67@gmail.com>
2023-11-02 03:55:10 +0400

    first.js

---
+++ script.js
@@ -0,0 +1,45 @@
+//Section-1
+//1.Nihad ve Nail-in cheki ve boyunu deyishende saxla!
+
+let Nihad_weight=78;
+let Nihad_height=1.69;
+let Nail_weight = 92;
+let Nail_height= 1.95;
+
+//2.Onlarin BKI hesabla!
+
+first=Nihad_weight/(Nihad_height**2);
+
+second=Nail_weight/(Nail_height**2);
+
+//3.Nihad-in BHI-i Nailden boyukdurmu? Boolean-da saxla!
+
+ if(first > second){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+//Section-2
+//1.Nihad ve Nail-in cheki ve boyunu deyishende saxla!
+
+let Nihad1_weight=95;
+let Nihad1_height=1.88;
+let Nail1_weight = 85;
+let Nail1_height= 1.76;
+
+//2.Onlarin BKI hesabla!
+
+third=Nihad1_weight/(Nihad1_height**2);
+
+fourth=Nail1_weight/(Nail1_height**2);
+
+//3.Nihad-in BHI-i Nailden boyukdurmu? Boolean-da saxla!
+
+ if(third > fourth){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
